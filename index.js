1e09a81da5a3c345bdc15dcf684dca2dc85058a6
Jared <Jared Allard>
2016-03-16 02:06:54 -0700

    forwarder: initial setup!

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
  const markutils   = require('./lib/simplemarkdown.js');
 
  let DiscordClient = require('discord.io'),
+     fs            = require('fs'),
      request       = require('request'),
      express       = require('express'),
      localMessageTable = [{
@@ -20,6 +21,13 @@
      config;
 
  let serverStatus = 'down';
+
+ let confirmTable = {}
+ let forwardName  = {}
+
+ if(fs.existsSync('./config/forwardnames.json')) {
+   forwardName = require('./config/forwardnames.json');
+ }
 
  // load the config
  try {
@@ -89,19 +97,28 @@
   *
   * @returns {bool} success
   **/
- function sendMessage(bot, to, message, opts) {
+ function sendMessage(bot, to, message, opts, parser) {
    if(to === undefined) { // using object params.
-      to = bot.to;
+      to      = bot.to;
       message = bot.message;
-      opts = bot.opts;
+      opts    = bot.opts;
+      parser  = bot.parser;
 
       // shift bot.bot to bot.
       bot = bot.bot;
    }
 
+   let finalmessage = '';
+   if(parser !== 'safe') {
+     console.log('Using strict parser')
+     finalmessage =  markutils.parse(message, opts);
+   } else {
+     finalmessage = markutils.safeparse(message, opts);
+   }
+
    return bot.sendMessage({
      to: to,
-     message: markutils.parse(message, opts)
+     message: finalmessage
    })
  }
 
@@ -117,8 +134,21 @@
    let isExecute    = /(^|\s)execute ([\s\S]+)$/ig
    let isStatus     = /(^|\s)status/ig;
 
+   let isConfirm    = /(^|\s)confirm/ig;
+   let isDeny       = /(^|\s)deny/ig;
+
+   if(getUserByID(bot, config.main, userID) === 'digitbot') {
+     return;
+   }
+
    if(channelID !== config.channel && config.channel !== '*') {
      return; // ignore non channels in the filter
+   }
+
+   if(channelID === config.forward) {
+     if(!isMention.test(message) && !isMentionOld.test(message)) {
+       forwardToMc(userID, message);
+     }
    }
 
    // push into message cache.
@@ -131,6 +161,60 @@
    // check if it's a mention.
    if(!isMention.test(message) && !isMentionOld.test(message)) {
     return; // ignore non mentions for now
+   }
+
+   if(isConfirm.test(message)) {
+     if(confirmTable[userID] !== undefined) {
+       if(confirmTable[userID].confirmed === false) {
+
+         confirmTable[userID].confirmed = true;
+         confirmTable[userID].action(true);
+
+         return sendMessage({
+           bot: bot,
+           to: channelID,
+           message: '<@{user}> Request has been {bold:"accepted"}!',
+           opts: {
+             user: userID
+           }
+         })
+       }
+     }
+
+     return sendMessage({
+       bot: bot,
+       to: channelID,
+       message: '<@{user}> Sorry, you have no outstanding confirmation requests!',
+       opts: {
+         user: userID
+       }
+     });
+   } else if(isDeny.test(message)) {
+     if(confirmTable[userID] !== undefined) {
+       console.log('[2A] Request Exists.')
+       if(confirmTable[userID].confirmed === false) {
+         confirmTable[userID].action(false);
+         confirmTable[userID] = undefined;
+
+         return sendMessage({
+           bot: bot,
+           to: channelID,
+           message: '<@{user}> Request has been {bold:"denied"}.',
+           opts: {
+             user: userID
+           }
+         })
+       }
+     }
+
+     return sendMessage({
+       bot: bot,
+       to: channelID,
+       message: '<@{user}> Sorry, you have no outstanding confirmation requests!',
+       opts: {
+         user: userID
+       }
+     });
    }
 
    // priveleged functions.
@@ -291,6 +375,178 @@
    }
  });
 
+const getUserByUsername = (bot, server, username) => {
+  let id = false;
+  const members = bot.servers[server].members;
+
+  // run over it.
+  Object.keys(members).forEach(function(v) {
+    const member = members[v];
+
+    if(member.user.username === username) {
+      id = member.user.id
+    }
+  });
+
+  return id;
+}
+
+const getUserByID = (bot, server, id) => {
+  let username = false;
+  const members = bot.servers[server].members;
+
+  // run over it.
+  Object.keys(members).forEach(function(v) {
+    const member = members[v];
+
+    if(member.user.id === id) {
+      username = member.user.username
+    }
+  });
+
+  return username;
+}
+
+const forwardToMc = (userID, message) => {
+  let username = getUserByID(bot, config.main, userID);
+
+  console.log(forwardName);
+
+  // translate ids to usernames
+  const matches = message.match(/<@([\d]+)>/g);
+
+  if(matches !== null) {
+    let finaltext = message;
+    matches.forEach(function(v) {
+      let id = v.replace('<@', '').replace('>', '');
+
+      finaltext = finaltext.replace(v, '@'+getUserByID(bot, config.main, id));
+    });
+
+    message = finaltext;
+  }
+
+  if(forwardName[userID] !== undefined) {
+    username = forwardName[userID].name
+  }
+
+  const cmd = 'say '+username+' said: '+message;
+  return sendAuthenticatedRequest('get', 'server/sendCommand/'+encodeURIComponent(cmd), function(err) {
+    if(err) {
+      return sendMessage({
+        bot: bot,
+        to: config.forward,
+        message: '<@{user}> Failed to relay the message: {err}',
+        opts: {
+          user: userID,
+          err: err
+        }
+      });
+    }
+  });
+}
+
+let forwardMessage = (data) => {
+  const message  = new Buffer(data.message, 'base64').toString('ascii');
+  const username = data.from;
+
+  let isMentionOld = new RegExp('^@discord');
+  let isIdent      = /\sidentity ([A-Z0-9]+)/ig
+
+  if(isMentionOld.test(message)) {
+    if(isIdent.test(message)) {
+      let willBe = /\sidentity ([A-Z0-9]+)/ig.exec(message)[1];
+
+      if(!willBe) return; // not supplied
+
+      let ID = getUserByUsername(bot, config.main, willBe);
+
+      if(forwardName[ID] !== undefined) {
+        if(forwardName[ID].name === username) {
+          return;
+        }
+      }
+
+      console.log(willBe, '=>', ID);
+
+      if(ID === false) return; // not a real user
+
+
+      const callback = (confirmed) => {
+        if(!confirmed) {
+          console.log('[2A] Callback: Denied.');
+          return;
+        }
+
+        console.log('[2A] Callback: Confirmed.')
+
+        forwardName[ID] = {
+          set: Date.now(),
+          name: username
+        }
+
+        fs.writeFileSync('./config/forwardnames.json', JSON.stringify(forwardName), 'utf8');
+      }
+
+      // add to the confirm table.
+      confirmTable[ID] = {
+        created: Date.now(),
+        confirmed: false,
+        action: callback,
+        link: username
+      };
+
+      return sendMessage({
+        bot: bot,
+        to: config.forward,
+        message: '<@{user}> Are you {bold:username}? Reply <@{ourID}> {bold:"confirm"} to confirm. Or <@{ourID}> {bold:"deny"} to deny.',
+        opts: {
+          user: ID,
+          ourID: getUserByUsername(bot, config.main, 'digitbot'),
+          username: username
+        }
+      })
+    }
+  }
+
+
+  const processAts = (text) => {
+    const matches = text.match(/@([\S]+)/ig);
+
+    let finaltext = text;
+
+    if(matches === null) {
+      console.log('No matches.')
+      return text;
+    }
+
+    matches.forEach(function(v) {
+      v = v.replace('@', '');
+
+      let replacewith = getUserByUsername(bot, config.main, v);
+
+      if(!replacewith) {
+        replacewith = '@'+v;
+      } else {
+        replacewith = '<@'+replacewith+'>'
+      }
+
+      finaltext = text.replace('@'+v, replacewith);
+    })
+
+    return finaltext;
+  }
+
+
+    // send the response.
+  return sendMessage({
+    bot: bot,
+    to: config.forward,
+    message: '<'+username+'> '+processAts(message),
+    opts: {},
+    parser: 'safe'
+  });
+}
 
 /* Express "webhook" */
 const app = express();
@@ -302,9 +558,14 @@ app.use(bodyP.json());
 app.use(morgan('dev'));
 
 app.post('/event', function(req, res) {
-  console.log(req.body);
-
   if(req.body.event === 'status') {
+    if(config.production === false) { // if not production, ignore.
+      return res.send({
+        success: true
+      });
+    }
+
+    // update our local cache of the status
     serverStatus = req.body.data;
 
     sendMessage({
@@ -315,6 +576,10 @@ app.post('/event', function(req, res) {
         status: serverStatus
       }
     });
+  }
+
+  if(req.body.event === 'chatMessage') {
+    forwardMessage(req.body.data);
   }
 
   res.send({
